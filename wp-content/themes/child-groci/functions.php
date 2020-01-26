d35f45b9b7e3eebd39582dffb0554159237593b3
rajadileepkumar <rajadileepkumar34@gmail.com>
2020-01-26 13:44:49 +0530

    Grocipal|Intial Commit for child theme

--- wp-content/themes/child-groci/functions.php
+++ wp-content/themes/child-groci/functions.php
@@ -1,4 +1,5 @@
 <?php 
+	 //load parent and child styles and scripts
 	 add_action( 'wp_enqueue_scripts', 'child_groci_enqueue_styles' );
 	 function child_groci_enqueue_styles() {
 		 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
