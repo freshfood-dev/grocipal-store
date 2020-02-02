a34ee656a44d9de63758f3403f2cac15c9072743
rajadileepkumar <rajadileepkumar34@gmail.com>
2020-02-02 10:50:43 +0530

    Grocipal|Account settings & Header fixes & layout

--- wp-content/themes/child-groci/functions.php
+++ wp-content/themes/child-groci/functions.php
@@ -1,9 +1,10 @@
 <?php 
-	 //load parent and child styles and scripts
 	 add_action( 'wp_enqueue_scripts', 'child_groci_enqueue_styles' );
 	 function child_groci_enqueue_styles() {
 		 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
 		 wp_enqueue_style( 'fonts', get_stylesheet_directory_uri() . '/assets/css/fonts.css' );
-		 wp_enqueue_style( 'text', get_stylesheet_directory_uri() . '/assets/css/text.css' ); 
+		 wp_enqueue_style( 'text', get_stylesheet_directory_uri() . '/assets/css/text.css' );
+		 wp_enqueue_style( 'responsive', get_stylesheet_directory_uri() . '/assets/css/responsive.css' ); 
+		 wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/assets/sass/main.css' ); 
  	} 
  ?>
